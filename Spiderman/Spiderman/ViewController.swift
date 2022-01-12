921eea389305a270a5ddfc07ae1f1d3cc2f82170
Navaneethan G <smashernavaneethan12@gmail.com>
2022-01-12 17:01:52 +0530

    storyboard initial ...

---
+++ Spiderman/Spiderman/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spiderman
+//
+//  Created by navaneethan-pt4855 on 12/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
